a95e03ee7d0c2e111ab3baec60a08dde5f3d5903
Ted Lyder <ted.lyder@skola.taby.se>
2025-11-07 09:32:25 +0100

    v 1.1 - bet fixes

--- SlotMachine.py
+++ SlotMachine.py
@@ -7,7 +7,16 @@ print("Skriv 'konto' för att se dina pengar.")
 print("Skriv 'r' för att rulla.")
 print("Skriv 'q' för att avsluta.")
 print(" ")
-bet = input("Hur mycket vill du betta?")
+
+while True:
+    bet = int(input("Hur mycket vill du betta? Du har: 200 Kr."))
+    if bet > konto or bet < 1:
+        print("Ogiltig bet! Försök igen.")
+        continue
+    else:
+        break
+
+print("Skriv 'bet' för att betta nytt senare.")
 
 while True:
     key = input().lower()
@@ -18,27 +27,46 @@ while True:
 
     if key == "q":
         break
+
+    elif key == "bet":      # byta bet
+        while True:
+            bet = input("Ny bettning: ")
+            if bet.isdigit() == True:                   # Kollar om bettningen är okej
+                if int(bet) > konto or int(bet) < 1:
+                    print("Ogiltig bet! Försök igen.")
+                    continue
+                else:
+                    print("Ändringen lyckades.")
+                    bet = int(bet)
+                    break
+            else:
+                print("Ogiltig bet! Försök igen.")
+                continue
+
     elif key == "r":
 
-        symboler = []
-        for i in range(3):
-            slot = random.choice(frukter)
-            symboler.append(slot)
-
-        if symboler[0] == symboler[1] == symboler[2]:
-            print(symboler, "\t🎉 JACKPOT! 🎉       1 in 64")
-            konto += int(bet) * 20
-        
-        elif symboler[0] == symboler[1] or symboler[1] == symboler[2] or symboler[0] == symboler[2]:
-            if symboler[0] == symboler[1] == symboler[2]:
-                break
-            else:
-                print(symboler, "\t+ ", int(bet) * 2, " Kr      1 in 3")
-                konto += int(bet) * 2
-
+        if bet > konto:
+            print("För stor bet. Ändra bet först.")
         else:
-            konto -= int(bet)      # drar pengar
-            print(symboler)
+            symboler = []
+            for i in range(3):
+                slot = random.choice(frukter)
+                symboler.append(slot)
+
+            if symboler[0] == symboler[1] == symboler[2]:
+                print(symboler, "\t🎉 JACKPOT! 🎉       1 in 64")
+                konto += int(bet) * 20
+            
+            elif symboler[0] == symboler[1] or symboler[1] == symboler[2] or symboler[0] == symboler[2]:
+                if symboler[0] == symboler[1] == symboler[2]:
+                    break
+                else:
+                    print(symboler, "\t+ ", int(bet) * 2, " Kr      1 in 3")
+                    konto += int(bet) * 2
+
+            else:
+                konto -= int(bet)      # drar pengar
+                print(symboler)
 
     elif key == "konto":
         print(str(konto) + str(" kr"))
